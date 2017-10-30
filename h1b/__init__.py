48da3cce462dd95b8562d55ddd9be5fed3943491
Mackenzie Gray <me@mackenziedg.com>
2017-10-29 21:31:29 -0400

    Initial bootstrap functionality.

--- h1b/__init__.py
+++ h1b/__init__.py
@@ -1,4 +1,17 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+nav = Nav()
+
+nav.register_element('top', Navbar(
+    View('Main', '.index'),
+    View('About', '.about')
+))
+
 app = Flask(__name__)
+Bootstrap(app)
+nav.init_app(app)
 
 import h1b.views
--- h1b/views.py
+++ h1b/views.py
@@ -4,5 +4,10 @@ from h1b import app
 
 # Create index view, just as a default
 @app.route('/')
-def show_index():
+def index():
     return render_template('index.html')
+
+
+@app.route('/about-us/')
+def about():
+    return render_template('about.html')
